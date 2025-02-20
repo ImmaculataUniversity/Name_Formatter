7ac2e9b24699fb4cffbb24e0a150f7a8db32ca13
maggiefriend <156940616+maggiefriend@users.noreply.github.com>
2025-02-20 08:43:03 -0500

    Update name_formatter_week5.py

--- name_formatter_week5.py
+++ name_formatter_week5.py
@@ -6,13 +6,15 @@ def get_formatted_name(first_name, middle_name = '', last_name =''):
     else:
         full_name = f"{first_name} {last_name}"
     return full_name.title()
-'''
+
+print('Example Musicians:')
 musician = get_formatted_name('jimi', 'hendrix')
 print(musician)
 
 musician = get_formatted_name('john', 'lee', 'hooker')
 print(musician)
-'''
+
+print()
 
 print('Name a musician:')
 first = input('First name: ')
